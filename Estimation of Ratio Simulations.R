5b34cb548467b42e100ded3376c7b9b6a2d63a46
jennn <kirk.jenn@gmail.com>
2015-01-30 16:24:11 -0800

    Added a bootstrap example with real data and removed the log ratio estimator from the simulations

--- Estimation of Ratio Simulations.R
+++ Estimation of Ratio Simulations.R
@@ -1,17 +1,36 @@
 library(MASS)
 library(ggplot2)
+setwd("~/Documents/Scott/ratioest")
+
+# Read in real data
+true.dat <- read.csv("results.csv", header = FALSE)
+hist(as.numeric(true.dat[2,]))
+
+res <- c()
+set.seed(98481)
+for(i in 1:5000)
+{
+  cont <- sample(as.numeric(true.dat[11,]), 50, replace = TRUE)
+  treat <- sample(as.numeric(true.dat[13,]), 50, replace = TRUE)
+  
+  res <- c(res, abs(mean(treat) - mean(cont))/mean(cont))
+}
+mt <- mean(as.numeric(true.dat[13,]))
+mc <- mean(as.numeric(true.dat[11,]))
+
+hist(res)
+abline(v = abs(mt-mc)/mc)
+
 # function to do one simulation
 sim.one <- function(r, cont.mean = 10)
 {
   # simulate time
-  cont <- rnegbin(n = 50, mu = cont.mean, theta = 20)
-  treat <- rnegbin(n = 50, mu = r*cont.mean, theta = 20)
+  cont <- rnegbin(n = 5, mu = cont.mean, theta = 20)
+  treat <- rnegbin(n = 5, mu = r*cont.mean, theta = 20)
   
-  # calculate the estimators
+  # calculate the estimator
   est <- abs(mean(treat) - mean(cont))/mean(cont)
-  est.log <- log(mean(treat)) - log(mean(cont))
-  
-  return(c(est, est.log))
+  return(est)
 }
 
 
@@ -19,26 +38,22 @@ sim.one <- function(r, cont.mean = 10)
 true.ratio <- seq(from = 1, to = 20, length.out = 100)
 
 set.seed(64684)
-out <- cbind(
-  rep(true.ratio[1], 2),
-             replicate(1000, sim.one(true.ratio[1])))
+out <- cbind(true.ratio[1], t(replicate(1000, sim.one(true.ratio[1]))))
 
 for(i in 2:length(true.ratio))
 {
   res <-replicate(1000, sim.one(true.ratio[i]))
-  out <- rbind(out,
-               cbind(rep(true.ratio[i], 2), res))  
+  out <- rbind(out, cbind(true.ratio[i], t(res)))  
 }
 
 out <- data.frame(out)
-out$method <- rep(c("ratio", "log"), 100)
 
-results <- data.frame(out[,1], out$method, rowMeans(out[,2:1001]))
-names(results) <- c("true.ratio", "method", "estimate")
+results <- data.frame(out[,1], rowMeans(out[,2:1001]))
+names(results) <- c("true.ratio", "estimate")
 
-dat <- subset(results, method == "ratio")
-dat$truth <- dat$true.ratio - 1
 
-plot <- ggplot(dat, aes(truth, estimate)) + geom_point() + theme_bw()
+results$truth <- dat$true.ratio - 1
+
+plot <- ggplot(results, aes(truth, estimate)) + geom_point() + theme_bw()
 plot <- plot + geom_abline(intercept = 0, slope = 1)
 plot
